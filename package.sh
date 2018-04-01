5991e2d4a22c08733287b96df93134e7cc9da55b
Anders Julfeldt Petersen <julfeldt@gmail.com>
2018-04-01 21:05:14 +0200

    finish

--- package.sh
+++ package.sh
@@ -2,7 +2,5 @@
 rm -rf package
 mkdir package
 cp -r build package
-cp -r fonts package
-cp -r images package
-cp -r css package
+cp -r assets package
 cp index.html package
--- src/main.js
+++ src/main.js
@@ -1,1 +1,103 @@
-import _ from "./ui";
+import { words, play, stopSound } from "./sound";
+
+const isTouchDevice = "ontouchstart" in document.documentElement;
+var sentences = document.querySelector("#sentence");
+
+let shouldPlay = false;
+
+// Disable scrolling on touch devices
+if (isTouchDevice) {
+    document.addEventListener("touchmove", function(e) {
+        e.preventDefault();
+    });
+}
+
+// Add draggable words
+for (var i = 0; i < words.length; i++) {
+    $(".container").append("<div class='item noselect draggable' data-word='" + i + "'>" + words[i].word + "</div>");
+}
+
+$(".draggable").draggable({
+    revert: true,
+    start: function() {
+        $(this).get(0).style.zIndex = 1000;
+    },
+    stop: function() {
+        $(this).get(0).style.zIndex = 1;
+    }
+});
+
+// Handle when a word is dragged to the sentence box
+$("#sentence").droppable({
+    drop: (event, ui) => {
+        var id = $(ui.draggable.context).data("word");
+        var obj = ui.draggable.context;
+
+        if ($(".item", "#sentence").length < 12) {
+            sentences.innerHTML += "<div class='item added' data-word='" + id + "'>" + obj.innerText + "</div>";
+            // Make it removable
+            $(".added").on("click", (e) => {
+                $(e.target).remove();
+            });
+        } else {
+            console.log("no more words!");
+            // TODO: say der er for mange ord!
+        }
+    }
+});
+
+const playButton = $("#play-button");
+
+
+// Play the whole setence
+playButton.on("click", (e) => {
+
+    // TBD - Understand this fully
+    // Handle errors
+
+    playButton.prop('disabled', true);
+
+    (async() => {
+        shouldPlay = true;
+        const words = $(".item", "#sentence");
+        for (var i = 0; i < words.length; i++) {
+            const word = words[i];
+            // previous
+            let prev = i > 0 ? i - 1 : 0;
+            $(words[prev]).removeClass("playing");
+            // current
+            $(words[i]).addClass("playing");
+            await playWordWithPromise($(word).data("word"));
+            if (i == words.length - 1) {
+                $(words[i]).removeClass("playing");
+            }
+        }
+        playButton.prop('disabled', false);
+    })();
+
+    function playWordWithPromise(id) {
+        return new Promise((resolve, reject) => {
+            if (shouldPlay) {
+                playWord(id, resolve, reject);
+            }
+        });
+    }
+});
+
+$("#clear-button").on("click", (e) => {
+    stopSound(true);
+    shouldPlay = false;
+    $(sentences).empty();
+    playButton.prop('disabled', false);
+});
+
+
+// Sample word
+$(".item").on("click", (e) => {
+    const id = $(e.target).data("word");
+    playWord(id);
+});
+
+const playWord = (id, onEnd) => {
+    play(words[id], onEnd);
+}
